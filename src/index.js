71221a0d26c5525653e917d5094d875b3d6df19f
Irina Shasheva <loyrina@gmail.com>
2019-05-03 01:13:42 +0300

    feat: create App and MainScreen components and render them from index.js entry point

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app/app.jsx';
+
+
+/** Entry point for project */
+const init = () => {
+
+  /** React render for App component */
+  ReactDOM.render(
+      <App/>,
+      document.querySelector(`#root`)
+  );
+};
+
+init();
