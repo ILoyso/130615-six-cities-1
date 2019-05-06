e19ff200a08b20000f39e78726908caae7ba37e9
Irina Shasheva <loyrina@gmail.com>
2019-05-06 21:31:05 +0300

    feat: add mock data for places

---
+++ src/mock/places.js
@@ -0,0 +1,34 @@
+export const PLACES_DATA = [
+  {
+    title: `Beautiful & luxurious apartment at great location`,
+    img: `apartment-01.jpg`,
+    type: `apartment`,
+    price: 120,
+    rating: 93,
+    isPremium: true
+  },
+  {
+    title: `Wood and stone place`,
+    img: `room.jpg`,
+    type: `room`,
+    price: 120,
+    rating: 80,
+    isPremium: false
+  },
+  {
+    title: `Canal View Prinsengracht`,
+    img: `apartment-02.jpg`,
+    type: `apartment`,
+    price: 132,
+    rating: 80,
+    isPremium: false
+  },
+  {
+    title: `Nice, cozy, warm big bed apartment`,
+    img: `apartment-03.jpg`,
+    type: `apartment`,
+    price: 180,
+    rating: 100,
+    isPremium: true
+  }
+];
