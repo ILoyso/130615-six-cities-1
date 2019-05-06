3406bcd8d09f93a86ec7330b8c0bcdba021114e2
Irina Shasheva <loyrina@gmail.com>
2019-05-07 00:12:58 +0300

    refactor: move getPlacesTemplate function from MainScreen component

--- src/components/main-screen/main-screen.jsx
+++ src/components/main-screen/main-screen.jsx
@@ -3,60 +3,59 @@ import PropTypes from "prop-types";
 
 
 /**
+ * Function for generating places template
+ * @param {Object} places
+ * @return {*}
+ */
+const getPlacesTemplate = (places) => {
+  return places.map((place, index) => {
+    const {img, isPremium, price, rating, title, type} = place;
+
+    return <article className="cities__place-card place-card" key={index}>
+      {isPremium && <div className="place-card__mark">
+        <span>Premium</span>
+      </div>}
+      <div className="cities__image-wrapper place-card__image-wrapper">
+        <a href="#">
+          <img className="place-card__image" src={`img/${img}`} width="260" height="200" alt="Place image" />
+        </a>
+      </div>
+      <div className="place-card__info">
+        <div className="place-card__price-wrapper">
+          <div className="place-card__price">
+            <b className="place-card__price-value">&euro;{price}</b>
+            <span className="place-card__price-text">&#47;&nbsp;night</span>
+          </div>
+          <button className="place-card__bookmark-button button" type="button">
+            <svg className="place-card__bookmark-icon" width="18" height="19">
+              <use xlinkHref="#icon-bookmark"></use>
+            </svg>
+            <span className="visually-hidden">To bookmarks</span>
+          </button>
+        </div>
+        <div className="place-card__rating rating">
+          <div className="place-card__stars rating__stars">
+            <span style={{width: `${rating}%`}}></span>
+            <span className="visually-hidden">Rating</span>
+          </div>
+        </div>
+        <h2 className="place-card__name">
+          <a href="#">{title}</a>
+        </h2>
+        <p className="place-card__type">{type}</p>
+      </div>
+    </article>;
+  });
+};
+
+
+/**
  * Component for main screen of application
  * @param {Object} props
  * @return {*}
  */
 const MainScreen = (props) => {
   const {places} = props;
-
-
-  /**
-   * Function for generating places template
-   * @param {Object} placesData
-   * @return {*}
-   */
-  const getPlacesTemplate = (placesData) => {
-    return placesData.map((place, index) => {
-      const {img, isPremium, price, rating, title, type} = place;
-
-      return <article className="cities__place-card place-card" key={index}>
-        {isPremium && <div className="place-card__mark">
-          <span>Premium</span>
-        </div>}
-        <div className="cities__image-wrapper place-card__image-wrapper">
-          <a href="#">
-            <img className="place-card__image" src={`img/${img}`} width="260" height="200" alt="Place image" />
-          </a>
-        </div>
-        <div className="place-card__info">
-          <div className="place-card__price-wrapper">
-            <div className="place-card__price">
-              <b className="place-card__price-value">&euro;{price}</b>
-              <span className="place-card__price-text">&#47;&nbsp;night</span>
-            </div>
-            <button className="place-card__bookmark-button button" type="button">
-              <svg className="place-card__bookmark-icon" width="18" height="19">
-                <use xlinkHref="#icon-bookmark"></use>
-              </svg>
-              <span className="visually-hidden">To bookmarks</span>
-            </button>
-          </div>
-          <div className="place-card__rating rating">
-            <div className="place-card__stars rating__stars">
-              <span style={{width: `${rating}%`}}></span>
-              <span className="visually-hidden">Rating</span>
-            </div>
-          </div>
-          <h2 className="place-card__name">
-            <a href="#">{title}</a>
-          </h2>
-          <p className="place-card__type">{type}</p>
-        </div>
-      </article>;
-    });
-  };
-
 
   return <>
     <div style={{display: `none`}}>
