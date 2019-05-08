ae54b33b5b3d7cabbb9de28d962ad2a3c29c39d1
Irina Shasheva <loyrina@gmail.com>
2019-05-08 15:24:55 +0300

    feat: add snapshot test for App component

---
+++ src/components/app/app.test.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import App from './app.jsx';
+
+const placesMock = [
+  {
+    img: `apartment-01.jpg`,
+    isPremium: true,
+    price: 120,
+    rating: 93,
+    title: `Beautiful & luxurious apartment at great location`,
+    type: `apartment`
+  }
+];
+
+it(`App correctly renders`, () => {
+  const tree = renderer
+    .create(<App
+      places={placesMock}
+    />)
+    .toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
