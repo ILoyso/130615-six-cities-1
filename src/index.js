0446173b81d719af3f5077eb8554ea358238ee7d
Irina Shasheva <loyrina@gmail.com>
2019-05-25 20:53:24 +0300

    feat: add Provider and connect it with store

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+
+import {reducer} from './reducer';
 import App from './components/app/app.jsx';
 
 import {PLACES_DATA} from "./mock/places";
@@ -7,13 +11,15 @@ import {PLACES_DATA} from "./mock/places";
 
 // Entry point for project
 const init = () => {
+  const store = createStore(reducer);
 
   // React render for App component
-  ReactDOM.render(
-      <App
-        places={PLACES_DATA}
-      />,
-      document.querySelector(`#root`)
+  ReactDOM.render(<Provider store={store}>
+    <App
+      places={PLACES_DATA}
+    />
+  </Provider>,
+  document.querySelector(`#root`)
   );
 };
 
