d5d49b8a41a4d961f8be20615ee51ac7c16dc658
Irina Shasheva <loyrina@gmail.com>
2019-05-25 21:02:00 +0300

    feat: add city to the places mock data

--- src/mock/places.js
+++ src/mock/places.js
@@ -1,5 +1,6 @@
 export const PLACES_DATA = [
   {
+    city: `Amsterdam`,
     coordinates: [52.3909553943508, 4.85309666406198],
     img: `apartment-01.jpg`,
     isPremium: true,
@@ -9,6 +10,7 @@ export const PLACES_DATA = [
     type: `apartment`
   },
   {
+    city: `Amsterdam`,
     coordinates: [52.369553943508, 4.85309666406198],
     img: `room.jpg`,
     isPremium: false,
@@ -18,6 +20,7 @@ export const PLACES_DATA = [
     type: `room`
   },
   {
+    city: `Amsterdam`,
     coordinates: [52.3909553943508, 4.929309666406198],
     img: `apartment-02.jpg`,
     isPremium: false,
@@ -27,6 +30,7 @@ export const PLACES_DATA = [
     type: `apartment`
   },
   {
+    city: `Amsterdam`,
     coordinates: [52.3809553943508, 4.939309666406198],
     img: `apartment-03.jpg`,
     isPremium: true,
@@ -34,5 +38,115 @@ export const PLACES_DATA = [
     rating: 100,
     title: `Nice, cozy, warm big bed apartment`,
     type: `apartment`
-  }
+  },
+  {
+    city: `Amsterdam`,
+    coordinates: [52.3808573943508, 4.974609666406198],
+    img: `apartment-03.jpg`,
+    isPremium: true,
+    price: 180,
+    rating: 100,
+    title: `Nice, cozy, warm big bed apartment`,
+    type: `apartment`
+  },
+  {
+    city: `Brussels`,
+    coordinates: [50.853585, 4.376354],
+    img: `apartment-03.jpg`,
+    isPremium: true,
+    price: 180,
+    rating: 100,
+    title: `Nice, cozy, warm big bed apartment`,
+    type: `apartment`
+  },
+  {
+    city: `Brussels`,
+    coordinates: [50.845287, 4.316633],
+    img: `apartment-03.jpg`,
+    isPremium: true,
+    price: 180,
+    rating: 100,
+    title: `Nice, cozy, warm big bed apartment`,
+    type: `apartment`
+  },
+  {
+    city: `Brussels`,
+    coordinates: [50.864956, 4.308865],
+    img: `apartment-03.jpg`,
+    isPremium: true,
+    price: 180,
+    rating: 100,
+    title: `Nice, cozy, warm big bed apartment`,
+    type: `apartment`
+  },
+  {
+    city: `Brussels`,
+    coordinates: [50.829299, 4.349649],
+    img: `apartment-03.jpg`,
+    isPremium: true,
+    price: 180,
+    rating: 100,
+    title: `Nice, cozy, warm big bed apartment`,
+    type: `apartment`
+  },
+  {
+    city: `Dusseldorf`,
+    coordinates: [51.260121, 6.802140],
+    img: `apartment-03.jpg`,
+    isPremium: true,
+    price: 180,
+    rating: 100,
+    title: `Nice, cozy, warm big bed apartment`,
+    type: `apartment`
+  },
+  {
+    city: `Dusseldorf`,
+    coordinates: [51.228144, 6.849567],
+    img: `apartment-03.jpg`,
+    isPremium: true,
+    price: 180,
+    rating: 100,
+    title: `Nice, cozy, warm big bed apartment`,
+    type: `apartment`
+  },
+  {
+    city: `Dusseldorf`,
+    coordinates: [51.200451, 6.821407],
+    img: `apartment-03.jpg`,
+    isPremium: true,
+    price: 180,
+    rating: 100,
+    title: `Nice, cozy, warm big bed apartment`,
+    type: `apartment`
+  },
+  {
+    city: `Hamburg`,
+    coordinates: [53.581753, 10.074024],
+    img: `apartment-03.jpg`,
+    isPremium: true,
+    price: 180,
+    rating: 100,
+    title: `Nice, cozy, warm big bed apartment`,
+    type: `apartment`
+  },
+  {
+    city: `Hamburg`,
+    coordinates: [53.553759, 9.923161],
+    img: `apartment-03.jpg`,
+    isPremium: true,
+    price: 180,
+    rating: 100,
+    title: `Nice, cozy, warm big bed apartment`,
+    type: `apartment`
+  },
+  {
+    city: `Paris`,
+    coordinates: [48.868735, 2.349666],
+    img: `apartment-03.jpg`,
+    isPremium: true,
+    price: 180,
+    rating: 100,
+    title: `Nice, cozy, warm big bed apartment`,
+    type: `apartment`
+  },
 ];
